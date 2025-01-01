0bd2c0c5588959deee5bf5c9c3d2849442d88026
hyun <jhc03032@naver.com>
2025-01-01 18:11:15 +0900

    img_url_change

--- src/app.js
+++ src/app.js
@@ -31,16 +31,16 @@ function createMessageBubble(content, sender = "user") {
 
     // 노트봇 이미지 아이콘
     const botImg = document.createElement("img");
-    botImg.src = "./assets/images/notebot_icon.png";
-    botImg.alt = "노트봇 아이콘";
+    botImg.src = "../assets/images/notebot_icon.png";
+    botImg.alt = "AI";
     botImg.classList.add("w-8", "h-8");
     avatar.appendChild(botImg);
   } else {
     avatar.classList.add("bg-gradient-to-br", "from-blue-500", "to-blue-700");
     
     const userImg = document.createElement("img");
-    userImg.src = "./assets/images/user_icon.png";
-    userImg.alt = "유저 아이콘";
+    userImg.src = "../assets/images/user_icon.png";
+    userImg.alt = "U";
     userImg.classList.add("w-8", "h-8");
     avatar.appendChild(userImg);
   }
