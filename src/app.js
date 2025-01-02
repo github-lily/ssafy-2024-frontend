2d2c5783d55ba5750c2ea345ff5a1b3eff3b0baa
hyun <jhc03032@naver.com>
2025-01-02 11:27:01 +0900

    app

--- src/app.js
+++ src/app.js
@@ -6,10 +6,9 @@ const userInput = document.getElementById("user-input");
 const apiSelector = document.getElementById("api-selector");
 const newChatBtn = document.getElementById("new-chat-btn");
 
-const BASE_URL = process.env.BACK_API_ENDPOINT;
+const BASE_URL = process.env.API_ENDPOINT;
 
 let db;
-
 
 async function initDB() {
   return new Promise((resolve, reject) => {
@@ -83,20 +82,11 @@ async function clearAllData() {
   });
 }
 
-// Create a message bubble
 function createMessageBubble(content, sender = "user") {
   const wrapper = document.createElement("div");
   wrapper.classList.add("mb-6", "flex", "items-start", "space-x-3");
 
-  // 개별 정렬 방향 설정
-  if (sender === "user") {
-    wrapper.classList.add("flex-row-reverse", "space-x-reverse", "space-x-3"); // Q는 오른쪽 정렬 // 수정!!
-  }
-
-  // Avatar
-  // 공통 프로필 아이콘 디자인
   const avatar = document.createElement("div");
-  console.log(avatar);
   avatar.classList.add(
     "w-10",
     "h-10",
@@ -109,21 +99,14 @@ function createMessageBubble(content, sender = "user") {
     "text-white"
   );
 
-  // 개별 프로필 아이콘 디자인
   if (sender === "assistant") {
-    avatar.classList.add("bg-gradient-to-br", "from-indigo-300", "to-blue-600");
+    avatar.classList.add("bg-gradient-to-br", "from-green-400", "to-green-600");
     avatar.textContent = "A";
   } else {
-    avatar.classList.add(
-      "bg-gradient-to-br",
-      "from-customPink",
-      "to-customSky700"
-    );
-    avatar.textContent = "Q";
+    avatar.classList.add("bg-gradient-to-br", "from-blue-500", "to-blue-700");
+    avatar.textContent = "U";
   }
 
-  // Bubble
-  // 공통 말풍선 디자인
   const bubble = document.createElement("div");
   bubble.classList.add(
     "max-w-full",
@@ -135,17 +118,12 @@ function createMessageBubble(content, sender = "user") {
     "shadow-sm"
   );
 
-  // 개별 말풍선 디자인
   if (sender === "assistant") {
-    bubble.classList.add("bg-customSky100", "text-gray-600");
+    bubble.classList.add("bg-gray-200", "text-gray-900");
   } else {
-    bubble.classList.add(
-      "bg-gradient-to-br",
-      "from-customSky500",
-      "to-customSky700",
-      "text-white"
-    );
+    bubble.classList.add("bg-blue-600", "text-white");
   }
+
   bubble.textContent = content;
 
   wrapper.appendChild(avatar);
@@ -153,11 +131,9 @@ function createMessageBubble(content, sender = "user") {
   return wrapper;
 }
 
-// Scroll to bottom
 function scrollToBottom() {
   chatContainer.scrollTop = chatContainer.scrollHeight;
 }
-
 
 async function getAssistantResponse(userMessage) {
   const mode = apiSelector.value;
